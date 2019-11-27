76dc93faf86b3ff1751756fe82bad0efa1ecbfbf
linxinhong <linxinhong.sky@gmail.com>
2019-11-27 12:30:07 +0800

    代码编辑内容支持强制识别为 autohotkey 语法

--- src/views/Menuz.vue
+++ src/views/Menuz.vue
@@ -1,54 +1,54 @@
 <template>
-    <div>
-        <a-row>
-            <a-col :span="8">
-                <MenuList />
-            </a-col>
-            <a-col :span="16">
-        <quill-editor id="editor" v-model="editorContext"  ref="myQuillEditor" :options="editorOption">
-        </quill-editor>
-            </a-col>
-        </a-row>
-    </div>
+  <div>
+    <a-row>
+      <a-col :span="8">
+        <MenuList />
+      </a-col>
+      <a-col :span="16">
+        <quill-editor
+          id="editor"
+          v-model="editorContext"
+          ref="myQuillEditor"
+          :options="editorOption"
+        ></quill-editor>
+      </a-col>
+    </a-row>
+  </div>
 </template>
 
 <script>
-import MenuList from '../components/MenuList'
-import hljs from 'highlight.js'
-import 'highlight.js/styles/monokai-sublime.css'
+import MenuList from "../components/MenuList";
+import hljs from "highlight.js";
+import "highlight.js/styles/monokai-sublime.css";
 
 hljs.configure({
-    languages: [
-        'autohotkey',
-        'json',
-        'yaml',
-        'ini',
-        'markdown'
-    ]
-})
+  languages: ["autohotkey", "json", "yaml", "ini", "markdown"]
+});
 
 export default {
-    data() {
-        return {
-            editorContext:'!z::reload',
-            editorOption: {
-                modules: {
-                    toolbar: [['code-block']],
-                    syntax: {
-                        highlight: text => hljs.highlightAuto(text, ['autohotkey']).value
-                }
-            }
-            }
+  data() {
+    return {
+      editorContext: "!z::reload",
+      editorOption: {
+        modules: {
+          toolbar: [
+            ["code-block"],
+          ],
+          syntax: {
+            highlight: text => hljs.highlightAuto(text, ["autohotkey"]).value
+          }
         }
-    },
-    components: {
-        MenuList
-    }
-}
+      }
+    };
+  },
+  components: {
+    MenuList
+  }
+};
 </script>
 
 <style>
-    #editor {
-        height: 500px;
-    }
+#editor {
+  height: 500px;
+}
 </style>
